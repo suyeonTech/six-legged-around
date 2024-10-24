48511aec98c6f2d87cd90b4349679974d9949dde
suyeonTech <wsy-ofc@naver.com>
2024-10-24 17:36:32 +0900

    new project

---
+++ src/main/java/com/sixleggedaround/SixLeggedAroundApplication.java
@@ -0,0 +1,13 @@
+package com.sixleggedaround;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SixLeggedAroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SixLeggedAroundApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sixleggedaround/SixLeggedAroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sixleggedaround;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SixLeggedAroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
